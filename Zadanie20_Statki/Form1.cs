dbb5f2c3d31593ecde93c48fa3c9d2798a56bca6
Mariusz Gaczyński <kontakt@mariuszgaczynski.com>
2020-05-19 16:30:47 +0200

    major changes

--- Zadanie20_Statki/Form1.cs
+++ Zadanie20_Statki/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,24 +19,37 @@ namespace Zadanie20_Statki
         }
 
 
-        List<Control> controls = new List<Control>();
+
+        public byte[,] locations = new byte[10, 10]; // tablica ze współrzędnymi lokacji
+
+        // List<Control> controls = new List<Control>();
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            
 
-           foreach (Control c in tableLayoutPanel.Controls)
-           {
-                controls.Add(c);
-                c.Tag = c.Text;
-                c.Name = "btn_" + c.Text;
-                c.Click += Btn_Click;
-                c.Text = "?";
-           }
-            
-            
-            ShipLocation(1);
+            BuildingShips4Masted();
+            BuildingShips3Masted();
+            BuildingShips3Masted();
+            BuildingShips2Masted();
+            BuildingShips2Masted();
+            BuildingShips2Masted();
+            BuildingShips1Masted();
+            BuildingShips1Masted();
+            BuildingShips1Masted();
+            BuildingShips1Masted();
 
+            //foreach (Control c in tableLayoutPanel.Controls)
+            //{
+            //     controls.Add(c);
+            //     c.Tag = c.Text;
+            //     c.TabIndex = Convert.ToInt32(c.Text);
+            //     c.Name = "btn_" + c.Text;
+            //     c.Click += Btn_Click;
+            //     c.Text = "?";
+            //}
+
+
+            // ShipLocation(4);
 
 
             // TableLayoutControlCollection
@@ -51,7 +65,6 @@ namespace Zadanie20_Statki
             //    }
 
 
-
             //int index = 1;
             //List<Control> controls = this.Controls.OfType<Button>().Cast<Control>().ToList();
             //controls.Reverse();
@@ -65,21 +78,424 @@ namespace Zadanie20_Statki
             //}
         }
 
+        
+        private void BuildingShips4Masted()
+        {
+            
+
+            byte xAxis = (byte)random.Next(0, 10);  // losowanie startowej pozycji 4masztowca
+            byte yAxis = (byte)random.Next(0, 10);
+
+            locations[xAxis, yAxis] = 4;       // przypisanie wartości 4masztowca lokalizacji startowej
+
+
+            bool shipCorrectLocation = false; // czy udało się umieścić cały okręt ?
+
+            do
+            {
+                int randomMethod = random.Next(1, 5);  // loswanie metody umieszczenia "reszty" okrętu
+
+
+                switch (randomMethod)
+                {
+                    case 1:
+                        {
+                            if (yAxis >= 3 
+                                && locations[xAxis, yAxis - 1] ==0 
+                                && locations[xAxis, yAxis - 2] == 0 
+                                && locations[xAxis, yAxis - 3] == 0)
+                            {
+                                locations[xAxis, yAxis - 1] = 4;
+                                locations[xAxis, yAxis - 2] = 4;
+                                locations[xAxis, yAxis - 3] = 4;
+                                shipCorrectLocation = true;
+                            }
+                            else
+                            {
+
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            if (yAxis <= 6
+                                && locations[xAxis, yAxis + 1] == 0
+                                && locations[xAxis, yAxis + 2] == 0
+                                && locations[xAxis, yAxis + 3] == 0)
+                            {
+                                locations[xAxis, yAxis + 1] = 4;
+                                locations[xAxis, yAxis + 2] = 4;
+                                locations[xAxis, yAxis + 3] = 4;
+                                shipCorrectLocation = true;
+                            }
+                            else
+                            {
+
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            if (xAxis >= 3
+                                && locations[xAxis -1, yAxis ] == 0
+                                && locations[xAxis -2, yAxis] == 0
+                                && locations[xAxis -3, yAxis ] == 0)
+                            {
+                                locations[xAxis - 1, yAxis] = 4;
+                                locations[xAxis - 2, yAxis] = 4;
+                                locations[xAxis - 3, yAxis] = 4;
+                                shipCorrectLocation = true;
+                            }
+                            else
+                            {
+
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            if (yAxis >= 6
+                                && locations[xAxis + 1, yAxis] == 0
+                                && locations[xAxis + 2, yAxis] == 0
+                                && locations[xAxis + 3, yAxis] == 0)
+                            {
+                                locations[xAxis + 1, yAxis] = 4;
+                                locations[xAxis + 2, yAxis] = 4;
+                                locations[xAxis + 3, yAxis] = 4;
+                                shipCorrectLocation = true;
+ 
+                            }
+                            else
+                            {
+
+                            }
+                            break;
+                        }
+
+                }
+
+
+            } while (shipCorrectLocation == false);
+
+        }
+
+
+
+        private void BuildingShips3Masted()
+        {
+            bool locationNotAvailable = false; // sprawdzam czy nie wpadłem na już istniejący okręt albo jego okolice
+            byte xAxis;
+            byte yAxis; 
+            do
+            {
+                 xAxis = (byte)random.Next(0, 10);  // losowanie startowej pozycji 3masztowca
+                 yAxis = (byte)random.Next(0, 10);
+
+                if (locations[xAxis, yAxis] != 0)
+                {
+                    locationNotAvailable = true; // już coś tam jest , losowanie od nowa
+                }
+                else
+                {
+                    locations[xAxis, yAxis] = 3;       // przypisanie wartości 3masztowca lokalizacji startowej
+                    locationNotAvailable = false;
+                }
+
+            } while (locationNotAvailable == true);
+
+
+
+            bool shipCorrectLocation = false; // czy udało się umieścić cały okręt ?
+
+            do
+            {
+                int randomMethod = random.Next(1, 5);  // loswanie metody umieszczenia "reszty" okrętu
+
+
+                switch (randomMethod)
+                {
+                    case 1:
+                        {
+                            if (yAxis >= 2
+                                && locations[xAxis, yAxis - 1] == 0
+                                && locations[xAxis, yAxis - 2] == 0)
+                            {
+                                locations[xAxis, yAxis - 1] = 3;
+                                locations[xAxis, yAxis - 2] = 3;
+                                shipCorrectLocation = true;
+                            }
+                            else
+                            {
+
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            if (yAxis <= 7
+                                && locations[xAxis, yAxis + 1] == 0
+                                && locations[xAxis, yAxis + 2] == 0)
+                            {
+                                locations[xAxis, yAxis + 1] = 3;
+                                locations[xAxis, yAxis + 2] = 3;
+                                shipCorrectLocation = true;
+                            }
+                            else
+                            {
+
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            if (xAxis >= 2
+                                && locations[xAxis - 1, yAxis] == 0
+                                && locations[xAxis - 2, yAxis] == 0)
+                            {
+                                locations[xAxis - 1, yAxis] = 3;
+                                locations[xAxis - 2, yAxis] = 3;
+                                shipCorrectLocation = true;
+                            }
+                            else
+                            {
+
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            if (yAxis >= 7
+                                && locations[xAxis + 1, yAxis] == 0
+                                && locations[xAxis + 2, yAxis] == 0)
+                            {
+                                locations[xAxis + 1, yAxis] = 3;
+                                locations[xAxis + 2, yAxis] = 3;
+                                shipCorrectLocation = true;
+
+                            }
+                            else
+                            {
+
+                            }
+                            break;
+                        }
+
+                }
+
+
+            } while (shipCorrectLocation == false);
+
+        }
+
+
+
+
+        private void BuildingShips2Masted()
+        {
+            bool locationNotAvailable = false; // sprawdzam czy nie wpadłem na już istniejący okręt albo jego okolice
+            byte xAxis;
+            byte yAxis;
+            do
+            {
+                xAxis = (byte)random.Next(0, 10);  // losowanie startowej pozycji 2masztowca
+                yAxis = (byte)random.Next(0, 10);
+
+                if (locations[xAxis, yAxis] != 0)
+                {
+                    locationNotAvailable = true; // już coś tam jest , losowanie od nowa
+                }
+                else
+                {
+                    locations[xAxis, yAxis] = 2;       // przypisanie wartości 2masztowca lokalizacji startowej
+                    locationNotAvailable = false;
+                }
+
+            } while (locationNotAvailable == true);
+
+
+
+            bool shipCorrectLocation = false; // czy udało się umieścić cały okręt ?
+
+            do
+            {
+                int randomMethod = random.Next(1, 5);  // loswanie metody umieszczenia "reszty" okrętu
+
+
+                switch (randomMethod)
+                {
+                    case 1:
+                        {
+                            if (yAxis >= 1
+                                && locations[xAxis, yAxis - 1] == 0)
+                            {
+                                locations[xAxis, yAxis - 1] = 2;
+                                shipCorrectLocation = true;
+                            }
+                            else
+                            {
+
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            if (yAxis <= 8
+                                && locations[xAxis, yAxis + 1] == 0)
+                            {
+                                locations[xAxis, yAxis + 1] = 2;
+                                shipCorrectLocation = true;
+                            }
+                            else
+                            {
+
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            if (xAxis >= 1
+                                && locations[xAxis - 1, yAxis] == 0)
+                            {
+                                locations[xAxis - 1, yAxis] = 2;
+                                shipCorrectLocation = true;
+                            }
+                            else
+                            {
+
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            if (yAxis >= 8
+                                && locations[xAxis + 1, yAxis] == 0)
+                            {
+                                locations[xAxis + 1, yAxis] = 2;
+                                shipCorrectLocation = true;
+
+                            }
+                            else
+                            {
+
+                            }
+                            break;
+                        }
+
+                }
+
+
+            } while (shipCorrectLocation == false);
+
+        }
+
+        private void BuildingShips1Masted()
+        {
+            bool locationNotAvailable = false; // sprawdzam czy nie wpadłem na już istniejący okręt albo jego okolice
+            byte xAxis;
+            byte yAxis;
+            do
+            {
+                xAxis = (byte)random.Next(0, 10);  // losowanie startowej pozycji 1masztowca
+                yAxis = (byte)random.Next(0, 10);
+
+                if (locations[xAxis, yAxis] != 0)
+                {
+                    locationNotAvailable = true; // już coś tam jest , losowanie od nowa
+                }
+                else
+                {
+                    locations[xAxis, yAxis] = 1;       // przypisanie wartości 1masztowca lokalizacji startowej
+                    locationNotAvailable = false;
+                }
+
+            } while (locationNotAvailable == true);
+
+
+        }
+
         Random random = new Random((Int32)DateTime.Now.Ticks);
 
-        private void ShipLocation (byte shipSize)
-        {
-           int tagNumber =  random.Next(1, controls.Count);
-            foreach (Control c in controls)
-            {
-                if (Convert.ToInt32(c.Tag) == tagNumber)
-                {
-                    c.Tag = "Ship";
-                    controls.Remove(c);
-                    break;
-                }
-            }
-        }
+        //private void ShipLocation (byte shipSize)
+        //{
+          
+        //    int tagNumber =  random.Next(1, controls.Count +1);
+            
+
+        //    foreach (Control c in controls)
+        //    {
+        //        if (Convert.ToInt32(c.TabIndex) == tagNumber)
+        //        {
+        //            c.Tag = "Ship";
+                    
+        //            break;
+
+        //        }
+        //    }
+
+        //    if (shipSize == 4)
+        //    {
+        //        bool correctShipLocation = false;
+
+        //        while (correctShipLocation == false)
+        //        {
+
+        //            int whichMethod = random.Next(1, 2);
+
+        //            switch (whichMethod)
+        //            {
+        //                case 1:
+        //                    if (tableLayoutPanel.Controls[tagNumber-10].Tag.ToString() != "Ship"
+        //                        && tableLayoutPanel.Controls[tagNumber - 10].Tag.ToString() != "ShipZone"
+        //                        && tagNumber - 10 >0)
+        //                    {
+        //                        if (tableLayoutPanel.Controls[tagNumber - 20].Tag.ToString() != "Ship"
+        //                        && tableLayoutPanel.Controls[tagNumber - 20].Tag.ToString() != "ShipZone"
+        //                        && tagNumber - 20 > 0)
+        //                        {
+        //                            if (tableLayoutPanel.Controls[tagNumber - 30].Tag.ToString() != "Ship"
+        //                        && tableLayoutPanel.Controls[tagNumber - 30].Tag.ToString() != "ShipZone"
+        //                        && tagNumber - 30 > 0)
+        //                            {
+        //                                tableLayoutPanel.Controls[tagNumber - 10].Tag = "Ship";
+        //                                tableLayoutPanel.Controls[tagNumber - 20].Tag = "Ship";
+        //                                tableLayoutPanel.Controls[tagNumber - 30].Tag = "Ship";
+
+        //                                correctShipLocation = true;
+        //                                break;
+        //                            }
+        //                            else
+        //                            {
+
+        //                            }
+        //                        }
+        //                        else
+        //                        {
+
+        //                        }
+        //                    }
+        //                    else
+        //                    {
+
+        //                    }
+                                
+        //                    break;
+        //                case 2:
+        //                    break;
+        //                case 3:
+        //                    break;
+        //                case 4:
+        //                    break;
+        //                default:
+        //                    break;
+        //            }
+        //        }
+        //    }
+
+        //     void LocationNorth()
+        //    {
+
+        //    }
+        //}
 
         //private void Form1_Load(object sender, EventArgs e)
         //{
@@ -106,19 +522,21 @@ namespace Zadanie20_Statki
 
 
 
-        private void Btn_Click(object sender, EventArgs e)
-        {
-            Button btn = (Button)sender;
+        //private void Btn_Click(object sender, EventArgs e)
+        //{
+        //    Button btn = (Button)sender;
 
-            if (btn.Tag.ToString() == "Ship")
-            {
-                btn.BackColor = Color.DarkOrange;
-            }
+        //    if (btn.Tag.ToString() == "Ship")
+        //    {
+        //        btn.BackColor = Color.DarkOrange;
+        //    }
             
-            MessageBox.Show(btn.Tag.ToString());
+        //    MessageBox.Show(btn.Tag.ToString());
+        //    MessageBox.Show(btn.TabIndex.ToString());
+        //   // MessageBox.Show(btn.Tag.ToString());
 
-            btn.Enabled = false;
-        }
+        //    btn.Enabled = false;
+        //}
 
 
 
